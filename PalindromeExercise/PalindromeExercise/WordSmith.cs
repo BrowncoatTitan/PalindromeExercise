ccc054d2fa1337f8995a6722966aa664cbca920e
Taylor McGregor <mcgregor.taylor44@gmail.com>
2023-09-18 14:38:44 -0500

    inital commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+           string word1 = string.Empty;
+           for (int i = word.Length - 1; i >= 0; i--)
+            {
+                word1 += word[i];
+            }
+            if (word == word1) return true;
+            return false;
+        }
+    
+    }
+
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,18 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("mom", true)]
+        [InlineData("dad", true)]
+        [InlineData("taco", false)]
+        [InlineData("papa", false)]
 
-        }
+        public void IsAPalinedromeTest(string str, bool expected)
+        {
+            WordSmith instance = new WordSmith();
+            bool actual = instance.IsAPalindrome(str);
+
+        }  
     }
 }
